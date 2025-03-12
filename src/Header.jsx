3379cad9ea3868579a541b48d1746f9d2803014a
Naluume <watyekeleawalin@gmail.com>
2025-03-12 15:46:24 +0300

    Added image in the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,7 @@
 export default function Header() {
   return (
     <div className="header">
-      <img
-        src="src/assets/Images/Troll-Face.png"
-        alt=""
-        width={30}
-        height={29}
-      />
+      <img src="/Troll Face.png" alt="" width={30} height={29} />
 
       <h2>Meme Generator</h2>
     </div>
